5d16eb7bedc3a222e678af42a0c3e5bf1f133d73
Alikhan <alikhan.adilkhan@bk.ru>
2020-03-30 14:46:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var money = +prompt("Ваш бюджет на месяц?");
+var time = prompt("Введите дату в формате YYYY-MM-DD");
+
+
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a2 = +prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = +prompt("Во сколько обойдется?");
+
+appData.expenses[a1] = a2;
+
+alert(appData.budget / 30);
